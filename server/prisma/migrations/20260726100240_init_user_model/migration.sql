7cd49ed4e609e458694f8ae53748f3d07ea75d35
Christopher Nyirongo <christophernyirongo50@gmail.com>
2026-07-26 12:06:25 +0200

    Add User model and run first migration

---
+++ server/prisma/migrations/20260726100240_init_user_model/migration.sql
@@ -0,0 +1,30 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('BUYER', 'SELLER', 'ADMIN', 'SUPER_ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profileImage" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'BUYER',
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "status" TEXT NOT NULL DEFAULT 'active',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
